fcb172e934b4b94f51469e146c96041fa89d518d
bahadirsengun <bahadirsngun@gmail.com>
2023-09-30 00:39:32 +0300

    devsengun commit

---
+++ evimKibris/LoginScreen/registerPage.swift
@@ -0,0 +1,22 @@
+//
+//  registerPage.swift
+//  evimKibris
+//
+//  Created by Bahadır Sengun on 30.09.2023.
+//
+
+import UIKit
+
+class registerPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("hadibismillah")
+        
+    }
+    
+
+   
+
+}
