0a90d3a83ff32553b55c0296c203c7f406877b1f
Bircan Sezgin <bircansezgin@outlook.com.tr>
2023-09-30 00:32:00 +0300

    first commit

---
+++ evimKibris/LoginScreen/LoginPage.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  evimKibris
+//
+//  Created by Bircan Sezgin on 30.09.2023.
+//
+
+import UIKit
+
+class LoginPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+
+}
+
